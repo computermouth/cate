d603904d19ca8152ece0578f7056aeffc61b174e
Benjamin Young <byoung@fastly.com>
2022-09-13 13:07:48 -0700

    Switch to gist backing

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main(_req: Request) -> Result<Response, Error> {
 	
 	println!("ok");
 	
-	match Request::get("https://hurricane-horn-tip.glitch.me")
-		.send("glitch")
+	match Request::get("https://gist.githubusercontent.com/computermouth/7b5460b0c78b3ad018ff29e97733c2df/raw")
+		.send("gist")
 	{
 		Ok(res) => Ok(res),
 		Err(_) => Ok(Response::from_status(StatusCode::INTERNAL_SERVER_ERROR))
